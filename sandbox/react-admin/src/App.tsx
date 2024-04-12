ac3c811ae82c245f5c597fa35f44a3e5e39b6198
Sam Johnston <samj@samj.net>
2024-04-12 09:29:46 -0700

    react-admin: create UserList from ListGuesser

--- sandbox/react-admin/src/App.tsx
+++ sandbox/react-admin/src/App.tsx
@@ -1,10 +1,12 @@
 import {
   Admin,
-  Resource,
-  ListGuesser,
-  EditGuesser,
-  ShowGuesser,
+  Resource
 } from "react-admin";
+import { UserList } from "./users";
 import { dataProvider } from "./dataProvider";
 
-export const App = () => <Admin dataProvider={dataProvider}></Admin>;
+export const App = () => (
+<Admin dataProvider={dataProvider}>
+  <Resource name="users" list={UserList} />
+</Admin>
+);
---
+++ sandbox/react-admin/src/users.tsx
@@ -0,0 +1,16 @@
+import { Datagrid, EmailField, List, TextField } from 'react-admin';
+
+export const UserList = () => (
+    <List>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="name" />
+            <TextField source="username" />
+            <EmailField source="email" />
+            <TextField source="address.street" />
+            <TextField source="phone" />
+            <TextField source="website" />
+            <TextField source="company.name" />
+        </Datagrid>
+    </List>
+);
