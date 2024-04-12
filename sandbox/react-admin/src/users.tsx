e2885a48065e2ae6a28ad8de83c958bea7e4fb00
Sam Johnston <samj@samj.net>
2024-04-12 09:32:21 -0700

    Datagrid -> SimpleList

--- sandbox/react-admin/src/users.tsx
+++ sandbox/react-admin/src/users.tsx
@@ -1,16 +1,11 @@
-import { Datagrid, EmailField, List, TextField } from 'react-admin';
+import { List, SimpleList } from 'react-admin';
 
 export const UserList = () => (
     <List>
-        <Datagrid rowClick="edit">
-            <TextField source="id" />
-            <TextField source="name" />
-            <TextField source="username" />
-            <EmailField source="email" />
-            <TextField source="address.street" />
-            <TextField source="phone" />
-            <TextField source="website" />
-            <TextField source="company.name" />
-        </Datagrid>
+        <SimpleList
+            primaryText={record => record.name}
+            secondaryText={record => record.username}
+            tertiaryText={record => record.email}
+        />
     </List>
 );
