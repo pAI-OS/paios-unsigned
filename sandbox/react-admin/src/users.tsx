f5066929fae974f30f28121746bca2b527943991
Sam Johnston <samj@samj.net>
2024-04-12 09:36:07 -0700

    react-admin: make it responsive

--- sandbox/react-admin/src/users.tsx
+++ sandbox/react-admin/src/users.tsx
@@ -1,11 +1,28 @@
-import { List, SimpleList } from 'react-admin';
+import { useMediaQuery, Theme } from "@mui/material";
+import { List, SimpleList, Datagrid, TextField, EmailField } from "react-admin";
 
-export const UserList = () => (
-    <List>
-        <SimpleList
-            primaryText={record => record.name}
-            secondaryText={record => record.username}
-            tertiaryText={record => record.email}
-        />
-    </List>
-);
+export const UserList = () => {
+    const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
+    return (
+        <List>
+            {isSmall ? (
+                <SimpleList
+                    primaryText={(record) => record.name}
+                    secondaryText={(record) => record.username}
+                    tertiaryText={(record) => record.email}
+                />
+            ) : (
+                <Datagrid rowClick="edit">
+                    <TextField source="id" />
+                    <TextField source="name" />
+                    <TextField source="username" />
+                    <EmailField source="email" />
+                    <TextField source="address.street" />
+                    <TextField source="phone" />
+                    <TextField source="website" />
+                    <TextField source="company.name" />
+                </Datagrid>
+            )}
+        </List>
+    );
+};
