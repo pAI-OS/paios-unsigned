3c310b124895c728ba0c32bb9e66282cf417028e
Sam Johnston <samj@samj.net>
2024-04-25 11:37:05 -0700

    added comments to clarify linkage to api.py via openapi.yaml

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,10 @@
 import connexion
 from flask_cors import CORS
 
+# Uses the connexion library to create a Flask app implmenting the OpenAPI
+# specification (../apis/paios/openapi.yaml), calling the python functions
+# in operationId (eg api.get_asset_by_id in api.py)
+
 app = connexion.App(__name__, specification_dir='../apis/paios/')
 CORS(app.app, expose_headers='X-Total-Count')
 
