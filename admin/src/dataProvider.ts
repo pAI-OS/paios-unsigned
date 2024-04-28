cf656b3452ca951eaeabd35d2671819e0a2871c7
Sam Johnston <samj@samj.net>
2024-04-27 18:30:45 -0700

    use default for VITE_JSON_SERVER_URL

--- admin/src/dataProvider.ts
+++ admin/src/dataProvider.ts
@@ -1,5 +1,5 @@
 import jsonServerProvider from "ra-data-json-server";
 
 export const dataProvider = jsonServerProvider(
-  import.meta.env.VITE_JSON_SERVER_URL
+  import.meta.env.VITE_JSON_SERVER_URL || 'http://localhost:3000'
 );
