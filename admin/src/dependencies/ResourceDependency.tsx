f68b1ece51eed4388f7431b39c4739b9e2960335
Sam Johnston <samj@samj.net>
2024-05-03 06:26:57 -0700

    implement downloads in frontent (progress, interruption todo)

--- admin/src/dependencies/ResourceDependency.tsx
+++ admin/src/dependencies/ResourceDependency.tsx
@@ -1,16 +1,65 @@
 // ResourceDependency.tsx
-import { Button, Datagrid, TextField } from 'react-admin';
+import { Button, Datagrid, TextField, useRecordContext, useNotify, useRefresh } from 'react-admin';
 import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
+import FileDownloadOffIcon from '@mui/icons-material/FileDownloadOff';
+import DeleteIcon from '@mui/icons-material/Delete'
+import { apiBase, httpClient } from "../apiBackend";
+
+const DownloadButton = ({ abilityId }: { abilityId: string }) => {
+    const record = useRecordContext();
+    const notify = useNotify();
+    const refresh = useRefresh();
+    const isDownloaded = false;
+
+    const handleDownloadClick = (event: React.MouseEvent) => {
+        // prevent the click event propagating to the row and calling show
+        event.stopPropagation();
+
+        httpClient(`${apiBase}/abilities/${abilityId}/dependencies/resources/${record.id}/download`, { method: 'POST' })
+            .then(() => {
+                notify('Download started');
+                refresh();
+            })
+            .catch((e) => {
+                notify('Error: download not started', { type: 'warning' })
+            });
+    };
+
+    const handleStopClick = (event: React.MouseEvent) => {
+        // prevent the click event propagating to the row and calling show
+        event.stopPropagation();
+
+        httpClient(`${apiBase}/abilities/${record.id}/stop`, { method: 'POST' })
+            .then(() => {
+                notify('Ability stopped');
+                refresh();
+            })
+            .catch((e) => {
+                notify('Error: ability not stopped', { type: 'warning' })
+            });
+    };
+
+    return !isDownloaded ? (
+        <Button label="Download" onClick={handleDownloadClick}>
+            <DownloadForOfflineIcon />
+        </Button>
+    ) : (
+        <Button label="Start" onClick={handleStopClick}>
+            <FileDownloadOffIcon />
+        </Button>
+    );
+};
 
 export const ResourceDependency = (props: { dependencies: any }) => {
+    const record = useRecordContext();
+    const abilityId = record.id;
+
     return (
         <Datagrid data={props.dependencies} sort={{ field: 'name', order: 'ASC' }}>
             <TextField source="name" />
             <TextField source="filename" />
             <TextField source="url" />
-            <Button label="Download" onClick={() => { /* handle download action */ }}>
-                <DownloadForOfflineIcon />
-            </Button>
+            <DownloadButton abilityId={abilityId} />        
         </Datagrid>
     );
 };
