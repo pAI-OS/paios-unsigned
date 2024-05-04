94e4b64efdb4b9f7d8ce1473524f755acc0bfa3c
Sam Johnston <samj@samj.net>
2024-05-04 13:16:44 -0700

    add airllm per doc

---
+++ abilities/airllm/main.py
@@ -0,0 +1,30 @@
+from airllm import AutoModel
+
+MAX_LENGTH = 128
+# could use hugging face model repo id:
+model = AutoModel.from_pretrained("garage-bAInd/Platypus2-70B-instruct")
+
+# or use model's local path...
+#model = AutoModel.from_pretrained("/home/ubuntu/.cache/huggingface/hub/models--garage-bAInd--Platypus2-70B-instruct/snapshots/b585e74bcaae02e52665d9ac6d23f4d0dbc81a0f")
+
+input_text = [
+        'What is the capital of United States?',
+        #'I like',
+    ]
+
+input_tokens = model.tokenizer(input_text,
+    return_tensors="pt", 
+    return_attention_mask=False, 
+    truncation=True, 
+    max_length=MAX_LENGTH, 
+    padding=False)
+           
+generation_output = model.generate(
+    input_tokens['input_ids'].cuda(), 
+    max_new_tokens=20,
+    use_cache=True,
+    return_dict_in_generate=True)
+
+output = model.tokenizer.decode(generation_output.sequences[0])
+
+print(output)
