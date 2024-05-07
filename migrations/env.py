9ff3d6903447a509bc52c2456e32afc5b824f046
Sam Johnston <samj@samj.net>
2024-05-06 22:26:48 -0700

    +SQLModel + configure env.py

--- migrations/env.py
+++ migrations/env.py
@@ -2,12 +2,19 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from pathlib import Path
+from sqlmodel import SQLModel
 
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+migrations_dir = Path(__file__).resolve().parent
+data_dir = migrations_dir.parent / "data"
+
+config.set_main_option("sqlalchemy.url", f"sqlite:///{data_dir / 'db.sqlite'}")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +25,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
