3b2ba8247d2889c868c0862c066eeed32d929b13
Sam Johnston <samj@samj.net>
2024-05-07 11:46:41 -0700

    venv->.venv

--- scripts/setup_environment.sh
+++ scripts/setup_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # setup_environment.sh
-python3 -m venv venv
-source venv/bin/activate
+python3 -m venv .venv
+source .venv/bin/activate
 pip3 install -r backend/requirements.txt
 cd admin
 npm install
