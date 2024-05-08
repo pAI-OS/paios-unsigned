c997246778b10a11e8204cf487682347631e7ba5
Sam Johnston <samj@samj.net>
2024-05-07 21:29:15 -0700

    move db functions to common db.py

--- backend/ConfigManager.py
+++ backend/ConfigManager.py
@@ -1,56 +1,22 @@
-import os
-import sqlite3
-from pathlib import Path
-from alembic import command
-from alembic.config import Config as AlembicConfig
+import db
 from EncryptionManager import EncryptionManager
-from paths import root_dir, db_path
+from paths import db_path
 
 class ConfigManager:
     def __init__(self, tenant=None):
         self.em = EncryptionManager()
         self.tenant = tenant
-        #self.backend_path = Path(__file__).resolve().parent
-        #self.base_path = self.backend_path.parent
-        #self.db_path = self.base_path / 'data' / (self.tenant if self.tenant else '')
-        #self.db_path = self.db_path / 'paios.db'
-        os.makedirs(db_path.parent, exist_ok=True)
-
-        # use alembic to create the database or migrate to the latest schema
-        self.init_db()
-
-    def init_db(self):
-        alembic_cfg = AlembicConfig()
-        alembic_cfg.set_main_option("script_location", str(root_dir / "migrations"))
-        alembic_cfg.set_main_option("sqlalchemy.url", f"sqlite:///{db_path}")
-        command.upgrade(alembic_cfg, "head")
-
-    def execute_query(self, query, params=None):
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-
-        try:
-            cursor.execute(query, params or ())
-            result = cursor.fetchall()
-            conn.commit()
-        except Exception as e:
-            conn.rollback()
-            raise e
-        finally:
-            cursor.close()
-            conn.close()
-
-        return result
+        db.init_db()
 
     # CRUD operations
     def create_config_item(self, key, value):
         encrypted_value = self.em.encrypt_value(value)
         query = 'INSERT INTO config (key, value) VALUES (?, ?)'
-        self.execute_query(query, (key, encrypted_value))
+        db.execute_query(query, (key, encrypted_value))
 
     def retrieve_config_item(self, key):
         query = 'SELECT value FROM config WHERE key = ?'
-        result = self.execute_query(query, (key,))
+        result = db.execute_query(query, (key,))
         if result:
             encrypted_value = result[0][0]
             return self.em.decrypt_value(encrypted_value)
@@ -59,8 +25,8 @@ class ConfigManager:
     def update_config_item(self, key, value):
         encrypted_value = self.em.encrypt_value(value)
         query = 'INSERT OR REPLACE INTO config (key, value) VALUES (?, ?)'
-        self.execute_query(query, (key, encrypted_value))
+        db.execute_query(query, (key, encrypted_value))
 
     def delete_config_item(self, key):
         query = 'DELETE FROM config WHERE key = ?'
-        self.execute_query(query, (key,))
+        db.execute_query(query, (key,))
---
+++ backend/db.py
@@ -0,0 +1,35 @@
+# database helper functions
+import os
+import sqlite3
+from alembic import command
+from alembic.config import Config as AlembicConfig
+from paths import root_dir, db_path
+
+# use alembic to create the database or migrate to the latest schema
+def init_db():
+    alembic_cfg = AlembicConfig()
+    #self.backend_path = Path(__file__).resolve().parent
+    #self.base_path = self.backend_path.parent
+    #self.db_path = self.base_path / 'data' / (self.tenant if self.tenant else '')
+    #self.db_path = self.db_path / 'paios.db'
+    os.makedirs(db_path.parent, exist_ok=True)
+    alembic_cfg.set_main_option("script_location", str(root_dir / "migrations"))
+    alembic_cfg.set_main_option("sqlalchemy.url", f"sqlite:///{db_path}")
+    command.upgrade(alembic_cfg, "head")
+
+def execute_query(query, params=None):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute(query, params or ())
+        result = cursor.fetchall()
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        raise e
+    finally:
+        cursor.close()
+        conn.close()
+
+    return result
