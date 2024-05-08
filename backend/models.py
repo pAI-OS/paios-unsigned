261c4268d26a95ef4d6f41acdf1511adc0a8f44e
Sam Johnston <samj@samj.net>
2024-05-08 12:28:03 -0700

    Channel and User fields should not be nullable

--- backend/models.py
+++ backend/models.py
@@ -6,10 +6,10 @@ class Config(SQLModel, table=True):
 
 class Channel(SQLModel, table=True):
     id: str = Field(default=None, primary_key=True)
-    name: str
-    uri: str
+    name: str = Field(nullable=False)
+    uri: str = Field(nullable=False)
 
 class User(SQLModel, table=True):
     id: str = Field(default=None, primary_key=True)
-    name: str
-    email: str
+    name: str = Field(nullable=False)
+    email: str = Field(nullable=False)
