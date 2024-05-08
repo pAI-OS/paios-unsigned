4b3f7599a0f3f99980408e165be97b64100691a4
Sam Johnston <samj@samj.net>
2024-05-08 11:36:04 -0700

    set {{PAIOS_BEARER_TOKEN}} for REST Client API testing from http files

---
+++ scripts/setup_vscode.sh
@@ -0,0 +1,15 @@
+# Get the absolute path to the script's parent directory
+PARENT_DIR=$(dirname "$(dirname "$(realpath "$0")")")
+
+# Define the source and target files
+SOURCE_FILE="$PARENT_DIR/.vscode/settings.json.sample"
+TARGET_FILE="$PARENT_DIR/.vscode/settings.json"
+
+# Load the VITE_PAIOS_BEARER_TOKEN from .env and substitute in settings.json.sample
+source "$PARENT_DIR/backend/.env"
+if [ -z "$PAIOS_BEARER_TOKEN" ]; then
+  echo "Warning: PAIOS_BEARER_TOKEN is not set in backend/.env."
+  echo "Please ensure to set PAIOS_BEARER_TOKEN by running the backend or manually setting it in backend/.env."
+else
+  sed "s/your_token_value_here/$PAIOS_BEARER_TOKEN/g" "$SOURCE_FILE" > "$TARGET_FILE"
+fi
