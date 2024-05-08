234707249e028eaa41a4fd071ed7cb6dd1eb187b
Sam Johnston <samj@samj.net>
2024-05-08 12:37:41 -0700

    move comments to paths.py

--- backend/db.py
+++ backend/db.py
@@ -8,10 +8,6 @@ from paths import root_dir, db_path
 # use alembic to create the database or migrate to the latest schema
 def init_db():
     alembic_cfg = AlembicConfig()
-    #self.backend_path = Path(__file__).resolve().parent
-    #self.base_path = self.backend_path.parent
-    #self.db_path = self.base_path / 'data' / (self.tenant if self.tenant else '')
-    #self.db_path = self.db_path / 'paios.db'
     os.makedirs(db_path.parent, exist_ok=True)
     alembic_cfg.set_main_option("script_location", str(root_dir / "migrations"))
     alembic_cfg.set_main_option("sqlalchemy.url", f"sqlite:///{db_path}")
