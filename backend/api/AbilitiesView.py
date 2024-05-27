d26a43f419f32cea8ca11764219ce550ba53439e
Sam Johnston <samj@samj.net>
2024-05-27 14:54:19 -0700

    implement MethodView structure

---
+++ backend/api/AbilitiesView.py
@@ -0,0 +1,15 @@
+class AbilitiesView:
+    def post(self, body: dict):
+        return "Hello, World!"
+
+    def put(self, body: dict):
+        return "Hello, World!"
+
+    def delete(self, abilityId: str):
+        return "Hello, World!"
+
+    def get(self, abilityId=None):
+        return "Hello, World!"
+
+    def search(limit=100):
+        return "Hello, World!"
---
+++ backend/api/__init__.py
@@ -0,0 +1,1 @@
+from .AbilitiesView import AbilitiesView
