337f2c57ebe4576e4ac6da1ddad457a606cdda33
Sam Johnston <samj@samj.net>
2024-05-28 10:30:14 -0700

    async UserManager

--- backend/UserManager.py
+++ backend/UserManager.py
@@ -1,36 +1,40 @@
 from uuid import uuid4
-import db
-from paths import db_path
+import backend.db as db
 
 class UserManager:
     def __init__(self):
         db.init_db()
 
-    def create_user(self, name, email):
+    async def create_user(self, name, email):
         id = str(uuid4())
         query = 'INSERT INTO user (id, name, email) VALUES (?, ?, ?)'
-        db.execute_query(query, (id, name, email))
+        await db.execute_query(query, (id, name, email))
         return id
 
-    def retrieve_all_users(self):
-        query = 'SELECT id, name, email FROM user'
-        results = db.execute_query(query)
+    async def retrieve_all_users(self, limit=None):
+        if limit is not None:
+            query = 'SELECT id, name, email FROM user LIMIT ?'
+            results = await db.execute_query(query, (limit,))
+        else:
+            query = 'SELECT id, name, email FROM user'
+            results = await db.execute_query(query)
+        
         users = []
         for result in results:
             users.append({'id': result[0], 'name': result[1], 'email': result[2]})
         return users
 
-    def retrieve_user(self, id):
+    async def retrieve_user(self, id):
         query = 'SELECT name, email FROM user WHERE id = ?'
-        result = db.execute_query(query, (id,))
+        result = await db.execute_query(query, (id,))
         if result:
             return {'id': id, 'name': result[0][0], 'email': result[0][1]}
         return None
 
-    def update_user(self, id, name, email):
+    async def update_user(self, id, name, email):
         query = 'INSERT OR REPLACE INTO user (id, name, email) VALUES (?, ?, ?)'
-        return db.execute_query(query, (id, name, email))
+        return await db.execute_query(query, (id, name, email))
 
-    def delete_user(self, id):
+    async def delete_user(self, id):
         query = 'DELETE FROM user WHERE id = ?'
-        return db.execute_query(query, (id,))
+        return await db.execute_query(query, (id,))
