a7680806c274c33a101f4e0ea3d0d172dd1db56e
Sam Johnston <samj@samj.net>
2024-05-28 10:31:03 -0700

    async UsersView

--- backend/api/UsersView.py
+++ backend/api/UsersView.py
@@ -1,30 +1,28 @@
-from uuid import uuid4
-from flask import jsonify, request
-import UserManager
+from starlette.responses import JSONResponse
+from backend.UserManager import UserManager
 
-um = UserManager.UserManager()
+class UsersView:
+    def __init__(self):
+        self.um = UserManager()
 
-def retrieve_all_users():
-    users = um.retrieve_all_users()
-    return jsonify(users), 200, {'X-Total-Count': len(users)}
+    async def get(self, userId: str):
+        user = await self.um.retrieve_user(userId)
+        if user is None:
+            return JSONResponse(status_code=404, headers={"error": "User not found"})
+        return JSONResponse(user, status_code=200)
 
-def retrieve_user_by_id(userId):
-    user = um.retrieve_user(userId)
-    if user is None:
-        return jsonify({"error": "User not found"}), 404
-    return jsonify(user), 200
+    async def post(self, body: dict):
+        user_id = await self.um.create_user(body['name'], body['email'])
+        return JSONResponse({"id": user_id}, status_code=201, headers={'Location': f'/users/{user_id}'})
+    
+    async def put(self, userId: str, body: dict):
+        await self.um.update_user(userId, body['name'], body['email'])
+        return JSONResponse({"message": "User updated successfully"}, status_code=200)
 
-def create_user():
-    user = request.get_json()
-    user['id'] = um.create_user(user['name'], user['email'])
-    # TODO: Return 409 if email already exists?
-    return jsonify(user), 201, {'Location': f'/users/{user["id"]}'}
-
-def update_user_by_id(userId):
-    data = request.get_json()
-    um.update_user(userId, data.get('name'), data.get('email'))
-    return jsonify({"message": "User updated successfully"}), 200
-
-def delete_user_by_id(userId):
-    um.delete_user(userId)
-    return '', 204
+    async def delete(self, userId: str):
+        await self.um.delete_user(userId)
+        return JSONResponse('', status_code=204)
+    
+    async def search(self, limit=100):
+        users = await self.um.retrieve_all_users(limit)
+        return JSONResponse(users, status_code=200, headers={'X-Total-Count': str(len(users))})
