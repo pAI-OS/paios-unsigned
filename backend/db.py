30e910dc416007ecdd01b90903f81c850b6ed63f
Sam Johnston <samj@samj.net>
2024-05-28 10:29:10 -0700

    async db interface with aiosqlite

--- backend/db.py
+++ backend/db.py
@@ -1,32 +1,29 @@
 # database helper functions
 import os
 import sqlite3
+import aiosqlite
 from alembic import command
 from alembic.config import Config as AlembicConfig
-from paths import root_dir, db_path
+from paths import base_dir, db_path
 
 # use alembic to create the database or migrate to the latest schema
 def init_db():
     alembic_cfg = AlembicConfig()
     os.makedirs(db_path.parent, exist_ok=True)
-    alembic_cfg.set_main_option("script_location", str(root_dir / "migrations"))
+    alembic_cfg.set_main_option("script_location", str(base_dir / "migrations"))
     alembic_cfg.set_main_option("sqlalchemy.url", f"sqlite:///{db_path}")
     command.upgrade(alembic_cfg, "head")
 
-def execute_query(query, params=None):
+async def execute_query(query, params=None):
     print(f"Executing query: {query} with params: {params}")
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-
-    try:
-        cursor.execute(query, params or ())
-        result = cursor.fetchall()
-        conn.commit()
-    except Exception as e:
-        conn.rollback()
-        raise e
-    finally:
-        cursor.close()
-        conn.close()
+    async with aiosqlite.connect(db_path) as conn:
+        async with conn.cursor() as cursor:
+            try:
+                await cursor.execute(query, params or ())
+                result = await cursor.fetchall()
+                await conn.commit()
+            except Exception as e:
+                await conn.rollback()
+                raise e
 
     return result
