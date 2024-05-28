6171fd8c43f7a4c48db614784690a497bbbbf59b
Sam Johnston <samj@samj.net>
2024-05-28 12:51:45 -0700

    nullable fields

--- backend/models.py
+++ backend/models.py
@@ -2,7 +2,7 @@ from sqlmodel import Field, SQLModel
 
 class Config(SQLModel, table=True):
     key: str = Field(default=None, primary_key=True)
-    value: str
+    value: str = Field(nullable=True)
 
 class Channel(SQLModel, table=True):
     id: str = Field(default=None, primary_key=True)
@@ -16,8 +16,8 @@ class User(SQLModel, table=True):
 
 class Asset(SQLModel, table=True):
     id: str = Field(default=None, primary_key=True)
+    user_id: str = Field(nullable=False)
     title: str = Field(nullable=False)
-    creator: str
-    subject: str
-    description: str
-
+    creator: str = Field(nullable=True)
+    subject: str = Field(nullable=True)
+    description: str = Field(nullable=True)
