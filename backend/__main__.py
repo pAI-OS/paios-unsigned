e46801de20acc5a167be8d13588aceeb537d81ff
Sam Johnston <samj@samj.net>
2024-05-28 14:08:31 -0700

    move OPTIONS handling to CORSMiddleware

--- backend/__main__.py
+++ backend/__main__.py
@@ -7,12 +7,31 @@ if base_dir not in sys.path:
     sys.path.append(str(base_dir))
 
 def create_backend_app():
-    import connexion
-    from connexion.resolver import MethodResolver
     from pathlib import Path
+    from connexion import AsyncApp
+    from connexion.resolver import MethodResolver
+    from connexion.middleware import MiddlewarePosition
+    from starlette.middleware.cors import CORSMiddleware
+
 
     apis_dir = Path(__file__).parent.parent / 'apis' / 'paios'
-    connexion_app = connexion.AsyncApp(__name__, specification_dir=apis_dir)
+    connexion_app = AsyncApp(__name__, specification_dir=apis_dir)
+
+    allow_origins = [
+        'http://localhost',
+        'http://localhost:3080',
+    ]
+
+   # Add CORS middleware
+    connexion_app.add_middleware(
+       CORSMiddleware,
+       position=MiddlewarePosition.BEFORE_EXCEPTION,
+       allow_origins=[allow_origins],  # TODO: Configure CORS
+       allow_credentials=True,
+       allow_methods=["*"],
+       allow_headers=["*"],
+       expose_headers=["X-Total-Count"],
+    )
 
     connexion_app.add_api('openapi.yaml', resolver=MethodResolver('backend.api'), resolver_error=501)
 
