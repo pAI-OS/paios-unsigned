94bc7917ff3185f76bd339dfe47cfbe2b951e718
Sam Johnston <samj@samj.net>
2024-05-28 10:28:56 -0700

    async ChannelManager

--- backend/ChannelManager.py
+++ backend/ChannelManager.py
@@ -1,33 +1,40 @@
-import db
-from paths import db_path
+from uuid import uuid4
+import backend.db as db
 
 class ChannelManager:
     def __init__(self):
         db.init_db()
 
-    def retrieve_all_channels(self):
-        query = 'SELECT id, name, uri FROM channel'
-        results = db.execute_query(query)
+    async def retrieve_all_channels(self, limit=None):
+        if limit is not None:
+            query = 'SELECT id, name, uri FROM channel LIMIT ?'
+            results = await db.execute_query(query, (limit,))
+        else:
+            query = 'SELECT id, name, uri FROM channel'
+            results = await db.execute_query(query)
+        
         channels = []
         for result in results:
             channels.append({'id': result[0], 'name': result[1], 'uri': result[2]})
         return channels
 
-    def create_channel(self, id, name, uri):
+    async def create_channel(self, name, uri):
+        id = str(uuid4())
         query = 'INSERT INTO channel (id, name, uri) VALUES (?, ?, ?)'
-        db.execute_query(query, (id, name, uri))
+        await db.execute_query(query, (id, name, uri))
+        return id
 
-    def retrieve_channel(self, id):
+    async def retrieve_channel(self, id):
         query = 'SELECT name, uri FROM channel WHERE id = ?'
-        result = db.execute_query(query, (id,))
+        result = await db.execute_query(query, (id,))
         if result:
             return {'id': id, 'name': result[0][0], 'uri': result[0][1]}
         return None
 
-    def update_channel(self, id, name, uri):
+    async def update_channel(self, id, name, uri):
         query = 'INSERT OR REPLACE INTO channel (id, name, uri) VALUES (?, ?, ?)'
-        db.execute_query(query, (id, name, uri))
+        await db.execute_query(query, (id, name, uri))
 
-    def delete_channel(self, id):
+    async def delete_channel(self, id):
         query = 'DELETE FROM channel WHERE id = ?'
-        db.execute_query(query, (id,))
+        await db.execute_query(query, (id,))
