a4ab366d6876db95ebf4f52024c97607fde1c950
Sam Johnston <samj@samj.net>
2024-05-28 10:28:24 -0700

    add api version

--- frontend/src/apiBackend.ts
+++ frontend/src/apiBackend.ts
@@ -1,6 +1,6 @@
 import { fetchUtils } from 'react-admin';
 
-export const apiBase = import.meta.env.VITE_JSON_SERVER_URL || 'http://localhost:3080';
+export const apiBase = import.meta.env.VITE_JSON_SERVER_URL || 'http://localhost:3080/api/v1';
 
 export const httpClient = (url: string, options: any = {}) => {
     if (!options.headers) {
