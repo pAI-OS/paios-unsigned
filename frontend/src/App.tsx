68320550b54d1d39185eb115abf44d6c25ef9013
Sam Johnston <samj@samj.net>
2024-05-28 16:25:40 -0700

    create a custom menu and add API Docs link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import {
   Admin,
-  Resource
+  Resource,
+  Menu
 } from "react-admin";
 import { UserList, UserShow } from "./users";
 import { AbilityList, AbilityShow } from "./abilities";
@@ -13,12 +14,38 @@ import ExtensionIcon from '@mui/icons-material/Extension';
 import SyncAltIcon from '@mui/icons-material/SyncAlt';
 import { Dashboard } from "./Dashboard";
 import { authProvider } from "./authProvider";
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ApiIcon from '@mui/icons-material/Api';
+import { apiBase } from './apiBackend';
+
+const CustomMenu = () => (
+  <Menu>
+    <Menu.DashboardItem />
+    <Menu.ResourceItem name="assets" />
+    <Menu.ResourceItem name="users" />
+    <Menu.ResourceItem name="abilities" />
+    <Menu.ResourceItem name="channels" />
+    <MenuItem component="a" href={`${apiBase}/ui`}>
+      <ListItemIcon>
+        <ApiIcon />
+      </ListItemIcon>
+      <ListItemText primary="API Docs" />
+    </MenuItem>
+  </Menu>
+);
 
 export const App = () => (
-<Admin dataProvider={dataProvider} authProvider={authProvider} dashboard={Dashboard} >
-  <Resource name="assets" list={AssetList} create={AssetCreate} edit={AssetEdit} recordRepresentation='title' icon={DocIcon} />
-  <Resource name="users" list={UserList} show={UserShow} recordRepresentation='name' icon={UserIcon} />
-  <Resource name="abilities" list={AbilityList} show={AbilityShow} recordRepresentation='id' icon={ExtensionIcon} />
-  <Resource name="channels" list={ChannelList} show={ChannelShow} recordRepresentation='id' icon={SyncAltIcon} />
-</Admin>
+  <Admin
+    dataProvider={dataProvider}
+    authProvider={authProvider}
+    dashboard={Dashboard}
+    menu={CustomMenu}
+  >
+    <Resource name="assets" list={AssetList} create={AssetCreate} edit={AssetEdit} recordRepresentation='title' icon={DocIcon} />
+    <Resource name="users" list={UserList} show={UserShow} recordRepresentation='name' icon={UserIcon} />
+    <Resource name="abilities" list={AbilityList} show={AbilityShow} recordRepresentation='id' icon={ExtensionIcon} />
+    <Resource name="channels" list={ChannelList} show={ChannelShow} recordRepresentation='id' icon={SyncAltIcon} />
+  </Admin>
 );
