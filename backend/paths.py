efeff6a51205f0c5c441ade2d1b09caa15faebb5
Sam Johnston <samj@samj.net>
2024-05-28 19:28:53 -0700

    tidy up paths

--- backend/paths.py
+++ backend/paths.py
@@ -1,16 +1,19 @@
 from pathlib import Path
 
-# TODO: Implement multi-tenancy e.g.:
-#self.backend_path = Path(__file__).resolve().parent
-#self.base_path = self.backend_path.parent
-#self.db_path = self.base_path / 'data' / (self.tenant if self.tenant else '')
-#self.db_path = self.db_path / 'paios.db'
-
-# directories
+# paios
 base_dir = Path(__file__).resolve().parent.parent
+backend_dir = base_dir / 'backend'
+frontend_dir = base_dir / 'frontend'
+env_file = backend_dir / '.env'
+
+# python venv
 venv_dir = base_dir / '.venv'
 venv_bin_dir = venv_dir / 'bin'
+
+# data
 data_dir = base_dir / 'data'
+
+# abilities
 abilities_subdir = 'abilities'
 abilities_dir = base_dir / abilities_subdir
 abilities_data_dir = data_dir / abilities_subdir
