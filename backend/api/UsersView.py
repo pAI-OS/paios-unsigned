d08b50d8f265eaf99a9e05d2999ae1367b92ce9a
Sam Johnston <samj@samj.net>
2024-05-28 22:11:08 -0700

    TODO react-admin GET?

--- backend/api/UsersView.py
+++ backend/api/UsersView.py
@@ -22,7 +22,8 @@ class UsersView:
     async def delete(self, userId: str):
         await self.um.delete_user(userId)
         return Response(status_code=204)
-    
+
+    # TODO: support react-admin GET /api/v1/users?id=5bae4a90-ce69-4483-86ef-d723258f21e5&id=432ec697-fd50-495b-bb35-cf0c9fcd92bb HTTP/1.1 ?
     async def search(self, limit=100):
         users = await self.um.retrieve_all_users(limit)
         return JSONResponse(users, status_code=200, headers={'X-Total-Count': str(len(users))})
