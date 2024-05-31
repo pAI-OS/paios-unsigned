53cfc60b083db535b421e15e18dd9c9b1d5cbbe3
Sam Johnston <samj@samj.net>
2024-05-31 13:47:21 -0700

    check_env to be a bit more user friendly

--- __main__.py
+++ __main__.py
@@ -1,11 +1,13 @@
 import sys
 from pathlib import Path
-from starlette.staticfiles import StaticFiles
 
 # Ensure the parent directory is in sys.path so relative imports work.
 base_dir = Path(__file__).parent
 if base_dir not in sys.path:
     sys.path.append(str(base_dir))
+
+# Requires sys.path to be set first
+from backend.env import check_env
 
 # Conditional import based on the running context (module or script)
 #try:
@@ -29,5 +31,13 @@ def create_app():
     return app
 
 if __name__ == "__main__":
+    # Check if the environment is set up and activated before importing dependencies
+    check_env()
+    from starlette.staticfiles import StaticFiles
+
+    # Create the app
+    app = create_app()
+
+    # Run the app
     import uvicorn
     uvicorn.run("__main__:create_app", host="localhost", port=3080, factory=True, workers=1)
