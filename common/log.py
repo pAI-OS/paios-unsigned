506366b9cf991034b110f4adba6c334933be8c7c
Sam Johnston <samj@samj.net>
2024-06-13 02:11:07 -0700

    no fallback to uvicorn's LOGGING_CONFIG

--- common/log.py
+++ common/log.py
@@ -1,24 +1,11 @@
-import json
 import logging.config
 import os
-import yaml
-import uvicorn.config
 from common.paths import log_dir
 from common.config import logging_config
 
 def setup_logging():
     os.makedirs(log_dir, exist_ok=True)
-    setup_logging_basic_config()
-
-def setup_logging_basic_config():
-    # uvicorn embeds a default logging config in config.py that is marginally better than nothing (though it may not configure the root logger)
-    try:
-        logging.config.dictConfig(logging_config)
-    except Exception as e:
-        print(f"Failed to load custom logging config '{logging_config}': {e}")
-        logging.config.dictConfig(uvicorn.config.LOGGING_CONFIG)
-    else:
-        logging.config.dictConfig(uvicorn.config.LOGGING_CONFIG)
+    logging.config.dictConfig(logging_config) # could fallback to uvicorn.config.LOGGING_CONFIG but requires import that may not be available pre-setup/check_env
 
 # Set up logging configuration once on first import only
 setup_logging()
